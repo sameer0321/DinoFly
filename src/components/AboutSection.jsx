83c5e2c0faf3e54051516c8c72ccc4dc4acfe6f5
sameer0321 <nitinsainsrowa@gmail.com>
2024-02-10 12:43:07 +0530

    rechecked

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -6,29 +6,31 @@ import rightDino from '../assets/images/right-dino.webp'
 const AboutSection = () => {
     return (
         <>
-            <div className="z-[unset] bg-[#09655A] relative sm:pt-20 pt-10 -mt-1" id="about">
-                <img
-                    src={rightTree} alt="Right-Tree"
-                    className="absolute right-0 lg:bottom-[-6%] sm:bottom-[-14%] bottom-0 z-[1] lg:max-w-[270px] sm:max-w-[200px] max-w-[120px]" />
-                <img
-                    src={leftTree} alt="left-Tree"
-                    className=" absolute lg:bottom-[-33%] sm:bottom-[-12%] bottom-0 left-0 z-[1] w-full lg:max-w-[197px] sm:max-w-[120px] max-w-[80px]" />
-                <img
-                    src={rightDino} alt="right-dino"
-                    className="absolute bottom-[-18%] right-[0] z-[1] lg:max-w-[170px] sm:max-w-[130px] max-w-[90px] w-full vertical_motion " />
-                <div className="container max-w-[1156px] relative mx-auto px-3 z-[1]">
-                    <h2
-                        className="font-normal sm:text-[69.669px] text-[42px] ff-dino drop-shadow-[0px_2.38px_0px_#FFF] leading-[113%] sm:mb-[42px] mb-6 text-center"><span className="ff-chewy text-white">About</span> <span className="text-[#FBA11D]">D</span><span
-                            className="text-[#23AAAC]">i</span><span className="text-[#D45B07]">n</span><span className="text-[#8EC627]">o</span><span
-                                className="text-[#FCCA2B]">L</span><span className="text-[#8F78D2]">F</span><span className="text-[#8EC627]">G</span></h2>
-                    <p className="text-white text-center sm:text-[24px] text-[16px] font-normal leading-[121%] mb-[18px] ff-balsamiq">$DINO is a meme coin with very strong utility.</p>
-                    <p className="text-white text-center ff-balsamiq mx-auto sm:text-[24px] text-[16px] lg:max-w-[560px] leading-[127%] mb-[10px] font-normal">Biggest problem of meme coins is lack of utility and usecase, <span
-                        className="text-[#8EC627] font-bold ">$</span><span className="text-[#FBA11D] font-bold">D</span><span
-                            className="text-[#23AAAC] font-bold">I</span><span className="text-[#D45B07] font-bold">N</span><span
-                                className="text-[#FB5352] font-bold">O</span> is going to fix it.</p>
-                    <p className="text-white mx-auto text-center font-normal sm:text-[24px] text-[16px] lg:max-w-[560px] leading-[127%] mb-3.5 ff-balsamiq">We want to bring a lot of usecases and utility to $DINO holders, just sit back and relax.</p>
-                    <p className="text-white sm:text-[24px] text-[16px] font-normal lg:max-w-[599px] text-center leading-[121%] md:pb-5 sm:pb-9 pb-7 ff-balsamiq mx-auto">In $DINO, we want to build a very strong community, that’s why we are going to have a lot of
-                        games and crypto tips in our Telegram and Twitter.</p>
+            <div className="z-[unset] bg-[#09655A] sm:pt-20 pt-10 -mt-1" id="about">
+                <div className="relative">
+                    <img
+                        src={rightTree} alt="Right-Tree"
+                        className="absolute right-0 lg:bottom-[-6%] sm:bottom-[-14%] bottom-0 z-[1] lg:max-w-[270px] sm:max-w-[200px] max-w-[120px]" />
+                    <img
+                        src={leftTree} alt="left-Tree"
+                        className=" absolute lg:bottom-[-33%] sm:bottom-[-12%] bottom-0 left-0 z-[1] w-full lg:max-w-[197px] sm:max-w-[120px] max-w-[80px]" />
+                    <img
+                        src={rightDino} alt="right-dino"
+                        className="absolute bottom-[-18%] right-[0] z-[1] lg:max-w-[170px] sm:max-w-[130px] max-w-[90px] w-full vertical_motion " />
+                    <div className="container max-w-[1156px] relative mx-auto px-3 z-[1]">
+                        <h2
+                            className="font-normal sm:text-[69.669px] text-[42px] ff-dino drop-shadow-[0px_2.38px_0px_#FFF] leading-[113%] sm:mb-[42px] mb-6 text-center"><span className="ff-chewy text-white">About</span> <span className="text-[#FBA11D]">D</span><span
+                                className="text-[#23AAAC]">i</span><span className="text-[#D45B07]">n</span><span className="text-[#8EC627]">o</span><span
+                                    className="text-[#FCCA2B]">L</span><span className="text-[#8F78D2]">F</span><span className="text-[#8EC627]">G</span></h2>
+                        <p className="text-white text-center sm:text-[24px] text-[16px] font-normal leading-[121%] mb-[18px] ff-balsamiq">$DINO is a meme coin with very strong utility.</p>
+                        <p className="text-white text-center ff-balsamiq mx-auto sm:text-[24px] text-[16px] lg:max-w-[560px] leading-[127%] mb-[10px] font-normal">Biggest problem of meme coins is lack of utility and usecase, <span
+                            className="text-[#8EC627] font-bold ">$</span><span className="text-[#FBA11D] font-bold">D</span><span
+                                className="text-[#23AAAC] font-bold">I</span><span className="text-[#D45B07] font-bold">N</span><span
+                                    className="text-[#FB5352] font-bold">O</span> is going to fix it.</p>
+                        <p className="text-white mx-auto text-center font-normal sm:text-[24px] text-[16px] lg:max-w-[560px] leading-[127%] mb-3.5 ff-balsamiq">We want to bring a lot of usecases and utility to $DINO holders, just sit back and relax.</p>
+                        <p className="text-white sm:text-[24px] text-[16px] font-normal lg:max-w-[599px] text-center leading-[121%] md:pb-5 sm:pb-9 pb-7 ff-balsamiq mx-auto">In $DINO, we want to build a very strong community, that’s why we are going to have a lot of
+                            games and crypto tips in our Telegram and Twitter.</p>
+                    </div>
                 </div>
             </div>
         </>
--- src/components/Tekonomics.jsx
+++ src/components/Tekonomics.jsx
@@ -33,7 +33,7 @@ const Tekonomics = () => {
                     className="container relative z-[1] xl:max-w-[1147px] px-3 mx-auto sm:pt-[236px] pt-[80px] xl:pb-[540px] lg:pb-[400px] sm:pb-[350px] pb-[175px]"
                     id="Tokenomic">
                     <h3
-                        className="text-white font-normal ff_chewy sm:text-[64px] text-[42px] leading-[131%] text-center xl:mb-[150px] lg:mb-[60px] mb-[45px]">Tokenomics</h3>
+                        className="text-white font-normal ff-chewy sm:text-[64px] text-[42px] leading-[131%] text-center xl:mb-[150px] lg:mb-[60px] mb-[45px]">Tokenomics</h3>
                     <div className="flex items-center justify-center">
                         <div className="relative xl:max-w-[589.84px] lg:max-w-[515px] sm:max-w-[390px] max-w-[330px]">
                             <img
@@ -49,8 +49,8 @@ const Tekonomics = () => {
                             <div
                                 className="md:flex hidden absolute xl:top-[12%] top-[41%] xl:left-[-45%] lg:left-[-46%] left-[-40%] lg:gap-[26.11px] gap-[15px]">
                                 <div className="flex flex-col gap-[5px]">
-                                    <p className="text-[#FBA11D] leading-[119%] lg:text-[32px] text-[23px] font-normal ff_Balsamiq">333,333,333</p>
-                                    <p className="text-white leading-[138%] font-normal text-[24px] text-right ff_Balsamiq">Supply</p>
+                                    <p className="text-[#FBA11D] leading-[119%] lg:text-[32px] text-[23px] font-normal ff-balsamiq">333,333,333</p>
+                                    <p className="text-white leading-[138%] font-normal text-[24px] text-right ff-balsamiq">Supply</p>
                                 </div>
                                 <div className="border border-solid border-white border-r-0 border-b-0 h-[88px] w-[16px]"></div>
                             </div>
@@ -59,7 +59,7 @@ const Tekonomics = () => {
                                 <div className="border border-solid border-white border-l-0 border-b-0 h-[88px] w-[16px]"></div>
                                 <div className="flex flex-col gap-[5px]">
                                     <p
-                                        className="text-[#8EC627] lg:max-w-[217px] max-w-[164px] leading-[119%] lg:text-[32px] text-[23px] font-normal ff_Balsamiq">
+                                        className="text-[#8EC627] lg:max-w-[217px] max-w-[164px] leading-[119%] lg:text-[32px] text-[23px] font-normal ff-balsamiq">
                                         Liquidity lock for 1 year</p>
                                 </div>
                             </div>
@@ -67,19 +67,19 @@ const Tekonomics = () => {
                                 className="md:flex hidden absolute xl:bottom-[-45%] bottom-[-28%] xl:left-[42%] left-[38%] justify-center items-center flex-col gap-[17px]">
                                 <div className="bg-white h-[43px] w-[1px]"></div>
                                 <div className="flex flex-col gap-[5px]">
-                                    <p className="text-[#D45B07] max-w-[217px] leading-[119%] text-[32px] font-normal ff_Balsamiq">No Tax</p>
+                                    <p className="text-[#D45B07] max-w-[217px] leading-[119%] text-[32px] font-normal ff-balsamiq">No Tax</p>
                                 </div>
                             </div>
                             <div
                                 className="md:flex hidden absolute xl:bottom-[-27%] bottom-[-17%] left-[-27%] justify-center items-center gap-[17px]">
                                 <div className="flex flex-col gap-[5px]">
-                                    <p className="text-[#23AAAC] max-w-[217px] leading-[119%] lg:text-[32px] text-[23px] font-normal ff_Balsamiq">95%
+                                    <p className="text-[#23AAAC] max-w-[217px] leading-[119%] lg:text-[32px] text-[23px] font-normal ff-balsamiq">95%
                                         Uniswa</p>
                                     <p
-                                        className="text-[#23AAAC] max-w-[217px] text-right leading-[119%] lg:text-[32px] text-[23px] font-normal ff_Balsamiq">
+                                        className="text-[#23AAAC] max-w-[217px] text-right leading-[119%] lg:text-[32px] text-[23px] font-normal ff-balsamiq">
                                         5% Marketing</p>
                                     <p
-                                        className="text-white max-w-[217px] text-right leading-[119%] lg:text-[32px] text-[23px]  font-normal ff_Balsamiq">
+                                        className="text-white max-w-[217px] text-right leading-[119%] lg:text-[32px] text-[23px]  font-normal ff-balsamiq">
                                         Allocation</p>
                                 </div>
                                 <div className="border-white border border-solid h-[102px] w-[16px] border-b-0 border-r-0 skew-y-[-50deg] "></div>
@@ -87,43 +87,43 @@ const Tekonomics = () => {
                             <div
                                 className="md:flex hidden absolute xl:bottom-[-23%] bottom-[-18%] right-[-31%] justify-center items-center gap-[17px]">
                                 <div className="border-white border border-solid h-[102px] w-[10px] border-b-0 border-l-0 skew-y-[52deg]"></div>
-                                <p className="text-[#FB5352] leading-[119%] lg:text-[32px] text-[23px] font-normal ff_Balsamiq">No Mint Function</p>
+                                <p className="text-[#FB5352] leading-[119%] lg:text-[32px] text-[23px] font-normal ff-balsamiq">No Mint Function</p>
                             </div>
                             <div className="md:hidden flex justify-center items-center mt-12">
                                 <div className=" flex flex-col gap-[35px]">
                                     <div className="flex sm:gap-[32px] gap-6">
                                         <div className="w-[34px] h-[34px] border border-solid border-white rounded-md bg-[#FBA11D]"></div>
                                         <div className="flex flex-col gap-[5px]">
-                                            <p className="text-[#FBA11D] leading-[119%] sm:text-[32px] text-[20px] font-normal ff_Balsamiq">333,333,333
+                                            <p className="text-[#FBA11D] leading-[119%] sm:text-[32px] text-[20px] font-normal ff-balsamiq">333,333,333
                                             </p>
-                                            <p className="text-white leading-[138%] font-normal sm:text-[24px] text-[20px] ff_Balsamiq">Supply</p>
+                                            <p className="text-white leading-[138%] font-normal sm:text-[24px] text-[20px] ff-balsamiq">Supply</p>
                                         </div>
                                     </div>
                                     <div className="flex sm:gap-[32px] gap-6">
                                         <div className="w-[34px] h-[34px] border border-solid border-white rounded-md bg-[#23AAAC]"></div>
                                         <div className="flex flex-col gap-[5px]">
-                                            <p className="text-[#23AAAC] leading-[119%] sm:text-[32px] text-[20px] font-normal ff_Balsamiq">95% Uniswa, 5%
+                                            <p className="text-[#23AAAC] leading-[119%] sm:text-[32px] text-[20px] font-normal ff-balsamiq">95% Uniswa, 5%
                                                 Marketing</p>
-                                            <p className="text-white leading-[138%] sm:text-[32px] text-[20px] font-normal ff_Balsamiq">Allocation</p>
+                                            <p className="text-white leading-[138%] sm:text-[32px] text-[20px] font-normal ff-balsamiq">Allocation</p>
                                         </div>
                                     </div>
                                     <div className="flex sm:gap-[32px] gap-6 items-center">
                                         <div className="w-[34px] h-[34px] border border-solid border-white rounded-md bg-[#D45B07]"></div>
                                         <div className="flex flex-col gap-[5px]">
-                                            <p className="text-[#D45B07] leading-[119%] sm:text-[32px] text-[20px] font-normal ff_Balsamiq">No Tax</p>
+                                            <p className="text-[#D45B07] leading-[119%] sm:text-[32px] text-[20px] font-normal ff-balsamiq">No Tax</p>
                                         </div>
                                     </div>
                                     <div className="flex sm:gap-[32px] gap-6 items-center">
                                         <div className="w-[34px] h-[34px] border border-solid border-white rounded-md bg-[#FB5352]"></div>
                                         <div className="flex flex-col gap-[5px]">
-                                            <p className="text-[#FB5352] leading-[119%] sm:text-[32px] text-[20px] font-normal ff_Balsamiq">No Mint
+                                            <p className="text-[#FB5352] leading-[119%] sm:text-[32px] text-[20px] font-normal ff-balsamiq">No Mint
                                                 Function</p>
                                         </div>
                                     </div>
                                     <div className="flex sm:gap-[32px] gap-6 items-center">
                                         <div className="w-[34px] h-[34px] border border-solid border-white rounded-md bg-[#8EC627]"></div>
                                         <div className="flex flex-col gap-[5px]">
-                                            <p className="text-[#8EC627] leading-[119%] sm:text-[32px] text-[20px] font-norma ff_Balsamiq">Liquidity lock
+                                            <p className="text-[#8EC627] leading-[119%] sm:text-[32px] text-[20px] font-norma ff-balsamiq">Liquidity lock
                                                 for 1 year</p>
                                         </div>
                                     </div>
